05cf563565f5a8871b65d8fb18fba9c0ec513988
dashingshakeel <shkeelrauf@gmail.com>
2017-07-23 13:30:10 +0500

    Complete Login functions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login @user
+      flash[:success] = "welcome to page"
+
       redirect_to @user
     else
       render 'new'

33cf2dac1b92e3d329e1268ea4518d78ff7bb14f
dashingshakeel <shkeelrauf@gmail.com>
2017-07-23 12:44:24 +0500

    Sessions controller and new page made.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'welcome#index'
   resources :users
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
